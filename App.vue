e3c1c1811d98c859cafe763f46474767e35bf8cf
tschlatter <taisa.schlatter@gmail.com>
2022-03-10 13:18:11 +0100

    ButtonToggle ON/OFF works BUT sidepanel appears when toggle off

--- App.vue
+++ App.vue
@@ -3,12 +3,7 @@
 		{{ message }}
 
 		<div id="button-toggle">
-			<VueToggles
-				checked-text="Chat On"
-				unchecked-text=" Chat Off"
-				:value="isToggled"
-				@click="visible = !visible"
-			/>
+			<VueToggles checked-text="Chat On" unchecked-text=" Chat Off" :value="isToggled" @click="toggleChat" />
 		</div>
 
 		<div id="side-panel">
@@ -17,6 +12,10 @@
 			</slide-out>
 
 			<button @click="visible = true">Show</button>
+		</div>
+
+		<div>
+			<button v-on:click="warn('Form cannot be submitted yet.', $event)">Submit</button>
 		</div>
 	</div>
 </template>
@@ -32,15 +31,22 @@ export default {
 	},
 
 	methods: {
-		onClosing(e) {
-			// prevent close and wait
-			e.pause = true;
-			// close after 3 seconds
-			setTimeout(() => {
-				// assign true to close, do nothing or assign false to cancel close.
-				e.resume = true;
-			}, 3000);
+		toggleChat: function (event) {
+			this.isToggled = !this.isToggled;
+			if (event) {
+				this.visible = true;
+			}
+			//alert(message);
 		},
+		// onClosing(e) {
+		// 	// prevent close and wait
+		// 	e.pause = true;
+		// 	// close after 3 seconds
+		// 	setTimeout(() => {
+		// 		// assign true to close, do nothing or assign false to cancel close.
+		// 		e.resume = true;
+		// 	}, 3000);
+		//},
 	},
 };
 </script>

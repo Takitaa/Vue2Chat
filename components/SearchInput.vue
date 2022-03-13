350e4334306b671ea44c3da0eb4b05f462f87db5
tschlatter <taisa.schlatter@gmail.com>
2022-03-13 11:46:04 +0100

    Playground App / SearchInput

---
+++ components/SearchInput.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<input v-on:input="onChange" type="text" />  <!-- v-on = @  LISTENER-->
+</div>
+
+</template>
+
+<script>
+export default{
+name: "SearchInput",
+
+methods: {
+    onChange: function(event) {
+        //console.log(event.target.value);
+        const term = event.target.value
+        this.$emit("newTerm", term);
+    }
+}
+};
+</script>
+
+<style></style>
